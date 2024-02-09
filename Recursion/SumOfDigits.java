e9c8fbc1531d6d783699a465917c4ac7be2329fa
Abhishek Madaan <abhishekmadaan717@gmail.com>
2024-02-09 22:06:31 +0530

    sum of digits

---
+++ Recursion/SumOfDigits.java
@@ -0,0 +1,14 @@
+package leetcodeQuestions.Recursion;
+class SumOfDigits {
+        public static void main(String[] args) {
+                int ans = sumOfDigits(1342);
+                System.out.println(ans);
+        }
+
+        public static int sumOfDigits(int n) {
+                if (n/10==0) {
+                        return n;
+                }
+                return n%10+sumOfDigits(n/10);
+        }
+}
